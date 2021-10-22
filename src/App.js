9c0ce53dae4183d551c3e56e6c930934156ede72
Dan Possehl <danielbpossehl@gmail.com>
2021-10-22 16:28:10 -0600

    created flex boxes to place data

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import FlexBox from "./components/FlexBox";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <FlexBox />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FlexBox.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./FlexBox.css";
+
+function FlexBox() {
+  return (
+    <div className="container">
+      <div className="box-1"> A </div>
+      <div className="box-2"> B </div>
+      <div className="box-3"> C </div>
+    </div>
+  );
+}
+
+export default FlexBox;

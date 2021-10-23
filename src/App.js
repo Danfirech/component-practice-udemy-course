ff65050c0d359b90fcf3e34a825a755ba0f37dff
Dan Possehl <danielbpossehl@gmail.com>
2021-10-23 17:42:10 -0600

    printed data on app.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,40 @@ import FlexBox from "./components/FlexBox";
 import "./App.css";
 
 function App() {
+  const dansData = [
+    {
+      id: "1",
+      sticks: 11,
+      heads: 6,
+      headPhones: 9,
+      band: "Ponce",
+      date: new Date(2020, 1, 19),
+    },
+
+    {
+      id: "2",
+      sticks: 190,
+      heads: 60,
+      headPhones: 50,
+      band: "The Killers",
+      date: new Date(2021, 12, 25),
+    },
+
+    {
+      id: "3",
+      sticks: 3403,
+      heads: 722,
+      headPhones: 855,
+      band: "Cran Apple",
+      date: new Date(2021, 16, 4),
+    },
+  ];
+
   return (
-    <div className="App">
-      <FlexBox />
+    <div>
+      <FlexBox sticks={dansData[1].sticks} heads={dansData[1].heads}></FlexBox>
+      <FlexBox sticks={dansData[2].sticks} band={dansData[1].band}></FlexBox>
+      <FlexBox></FlexBox>
     </div>
   );
 }
--- src/components/FlexBox.js
+++ src/components/FlexBox.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./FlexBox.css";
 
-function FlexBox() {
+function FlexBox(props) {
   return (
     <div className="container">
-      <div className="box-1"> A </div>
-      <div className="box-2"> B </div>
-      <div className="box-3"> C </div>
+      <div className="box-1">
+        {props.sticks} {props.heads} {props.band}
+      </div>
     </div>
   );
 }

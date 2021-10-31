71367867de5e8d94ace01af62d67641edab9a428
Dan Possehl <danielbpossehl@gmail.com>
2021-10-31 15:40:11 -0600

    installed styled components and added first component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,7 @@
-import FlexBox from "./components/FlexBox";
-import "./App.css";
-
 function App() {
-  const dansData = [
-    {
-      id: "1",
-      sticks: 11,
-      heads: 6,
-      headPhones: 9,
-      band: "Ponce",
-      date: new Date(2020, 1, 19),
-    },
-
-    {
-      id: "2",
-      sticks: 190,
-      heads: 60,
-      headPhones: 50,
-      band: "The Killers",
-      date: new Date(2021, 12, 25),
-    },
-
-    {
-      id: "3",
-      sticks: 3403,
-      heads: 722,
-      headPhones: 855,
-      band: "Cran Apple",
-      date: new Date(2021, 16, 4),
-    },
-  ];
-
   return (
     <div>
-      <FlexBox sticks={dansData[1].sticks} heads={dansData[1].heads}></FlexBox>
-      <FlexBox sticks={dansData[2].sticks} band={dansData[1].band}></FlexBox>
-      <FlexBox></FlexBox>
+      <h1>test test</h1>
     </div>
   );
 }
--- src/components/ExpenseDate.js
+++
@@ -1,5 +0,0 @@
-function ExpensDate(props) {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-us", { day: "2-digit" });
-  const year = props.daye.getFullyYear();
-}
